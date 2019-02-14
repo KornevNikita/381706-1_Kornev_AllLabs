d16963a6e7b25809c7bd8f8507c4e4d535af27c5
KornevNikita <copperskylark@gmail.com>
2019-02-14 10:33:57 +0300

    little edits

--- ListLib/List.h
+++ ListLib/List.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "Elem.h"
 #include "MyException.h"
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
 
 using namespace std;
 
@@ -9,16 +10,16 @@ template <class T>
 class TList 
 {
 protected:
-  TElem<T>* begin; // Указатель на первое звено списка
+TElem<T>* begin; // Указатель на первое звено списка
 public:
-  TList(); // Конструктор по умолчанию
-  TList(TList<T> &A); // Конструктор копирования
-  void PutBegin(T A); // Установить начальное звено
-  void PutEnd(T A); // Установить последнее звено
-  T GetBegin(); // Взять начало
-  T GetEnd(); // Взять конец
-  bool IsFull(); // Проверка на полноту
-  bool IsEmpty(); // Проверка на пустоту
+TList(); // Конструктор по умолчанию
+TList(TList<T> &A); // Конструктор копирования
+void PutBegin(T A); // Установить начальное звено
+void PutEnd(T A); // Установить последнее звено
+T GetBegin(); // Взять начало
+T GetEnd(); // Взять конец
+bool IsFull(); // Проверка на полноту
+bool IsEmpty(); // Проверка на пустоту
 };
 
 template <class T>
@@ -32,18 +33,20 @@ TList<T>::TList(TList<T> &A)
 {
   TElem<T> *tmp1 = A.begin;
   TElem<T> *tmp2;
+  
   if (A.begin == 0)
-	  begin = 0;
+	begin = 0;
+  
   else
   {
-	  begin = new TElem<T>(*A.begin);
-	  tmp2 = begin;
-	  while (tmp1->TElem<T>::GetNext() != 0)
-	  {
-		  tmp2->TElem<T>::SetNext(new TElem<T>(*(tmp1->TElem<T>::GetNext())));
-		  tmp1 = tmp1->TElem<T>::GetNext();
-		  tmp2 = tmp2->TElem<T>::GetNext();
-	  }
+	begin = new TElem<T>(*A.begin);
+	tmp2 = begin;
+	while (tmp1->TElem<T>::GetNext() != 0)
+	{
+	  tmp2->TElem<T>::SetNext(new TElem<T>(*(tmp1->TElem<T>::GetNext())));
+	  tmp1 = tmp1->TElem<T>::GetNext();
+	  tmp2 = tmp2->TElem<T>::GetNext();
+	}
   }
 }
 
@@ -52,60 +55,63 @@ void TList<T>::PutBegin(T A)
 {
   if (begin == 0) //Если список пуст
   {
-	  TElem<T>* temp = new TElem<T>(A, 0);
-	  begin = temp;
+	TElem<T>* temp = new TElem<T>(A, 0);
+	begin = temp;
   }
+
   else
   {
-	  TElem<T>* temp = new TElem<T>(A, begin);
-	  begin = temp;
+	TElem<T>* temp = new TElem<T>(A, begin);
+	begin = temp;
   }
 }
 
 template <class T>
 void TList<T>::PutEnd(T A)
 {
-  if (IsEmpty()){
-	  begin = new TElem<T>(A, 0);
-  }
-  else {
-	  TElem<T>* temp = begin;
-	  while (temp->GetNext() != 0)
-		  temp = temp->GetNext();
+  if (IsEmpty())
+	begin = new TElem<T>(A, 0);
 
-	  temp->SetNext(new TElem <T>(A, 0));
+  else 
+  {
+	TElem<T>* temp = begin;
+	while (temp->GetNext() != 0)
+		temp = temp->GetNext();
+
+	temp->SetNext(new TElem <T>(A, 0));
   }
 }
 
 template <class T>
 T TList<T>::GetBegin()
 {
-  if (!IsEmpty()) {
-	  T tempE = begin->Get();
-	  begin = begin->GetNext();
-	  return tempE;
+  if (!IsEmpty()) 
+  {
+    T tempE = begin->Get();
+	begin = begin->GetNext();
+	return tempE;
   }
-  else
-	  throw "Have not any elem";
+
+else
+  throw "Have not any elem";
 }
 
 template <class T>
 T TList<T>::GetEnd()
 {
-  if (!IsEmpty()) {
-	  TElem<T>* temp = begin;
-	  while ((temp->GetNext())->GetNext() != 0)
-		  temp = temp->GetNext();
+  if (!IsEmpty()) 
+  {
+	TElem<T>* temp = begin;
+	while ((temp->GetNext())->GetNext() != 0)
+	  temp = temp->GetNext();
+	T tempE = (temp->GetNext())->Get();
+	temp->SetNext(0);
 
-	  T tempE = (temp->GetNext())->Get();
-
-	  temp->SetNext(0);
-
-	  return tempE;
+	return tempE;
   }
-  else
-	  throw "Have not any elem";
 
+  else
+	throw "Have not any elem";
 }
 
 template <class T>
@@ -113,14 +119,13 @@ bool TList<T>::IsFull()
 {
   try
   {
-	  TElem<T>* A = new TElem<T>();
-	  return (A == 0);
+	TElem<T>* A = new TElem<T>();
+	return (A == 0);
   }
-  catch (...)
-  {
-	return false;
-  }	
-  return true;
+catch (...)
+  return false;
+
+return true;
 }
 
 template <class T>
--- VectorLib/Vector.h
+++ VectorLib/Vector.h
@@ -1,212 +1,261 @@
 #pragma once
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
 #include "MyException.h"
 
 template <class T>
-class TVector {
+class TVector 
+{
 protected:
-	int n;
-	T *v;
+  int n;
+  T *v;
 public:
-	TVector(int _n = 0);
-	TVector(const TVector &other);
-	virtual TVector& operator=(const TVector &other);
-	int GetSize() const { return n; }
-	T& operator[](int i);
-	virtual TVector operator+()const;
-	TVector operator++();
-	virtual TVector operator++(int);
-	virtual TVector operator-()const;
-	virtual TVector operator--();
-	virtual TVector operator--(int);
-	virtual TVector operator+(const TVector &other);
-	virtual TVector operator-(const TVector &other);
-	virtual TVector operator*(const TVector &other);
-	virtual TVector operator*(T ch);
-	virtual bool operator==(const TVector &other);
-	virtual bool operator!=(const TVector &other);
-	virtual ~TVector();
-	friend TVector<T> operator*(T ch, const TVector &other) {
-		TVector<T> temp(other.n);
-		for (int i = 0;i < other.n;i++)
-			temp.v[i] = ch * other.v[i];
-		return temp;
+  TVector(int _n = 0);
+  TVector(const TVector &other);
+  virtual TVector& operator=(const TVector &other);
+  int GetSize() const { return n; }
+  T& operator[](int i);
+  virtual TVector operator+()const;
+  TVector operator++();
+  virtual TVector operator++(int);
+  virtual TVector operator-()const;
+  virtual TVector operator--();
+  virtual TVector operator--(int);
+  virtual TVector operator+(const TVector &other);
+  virtual TVector operator-(const TVector &other);
+  virtual TVector operator*(const TVector &other);
+  virtual TVector operator*(T ch);
+  virtual bool operator==(const TVector &other);
+  virtual bool operator!=(const TVector &other);
+  virtual ~TVector();
+  
+  friend TVector<T> operator*(T ch, const TVector &other) 
+  {
+	TVector<T> temp(other.n);
+	for (int i = 0;i < other.n;i++)
+	  temp.v[i] = ch * other.v[i];
+	return temp;
+  }
+  
+  friend ostream &operator<<(ostream &os, const TVector other) 
+  {
+	for (int i = 0;i < other.n;i++)
+	  os << other.v[i] << "\t";
+	os << endl;
+	return os;
+  }
+  friend istream &operator>>(istream &is, TVector &other) 
+  {
+	cout << "TVector(n=" << other.n << "):" << endl;
+	for (int i = 0;i < other.n;i++) 
+	{
+	  cout << "[" << i << "]=";
+	  is >> other.v[i];
 	}
-	friend ostream &operator<<(ostream &os, const TVector other) {
-		for (int i = 0;i < other.n;i++)
-			os << other.v[i] << "\t";
-		os << endl;
-		return os;
-	}
-	friend istream &operator>>(istream &is, TVector &other) {
-		cout << "TVector(n=" << other.n << "):" << endl;
-		for (int i = 0;i < other.n;i++) {
-			cout << "[" << i << "]=";
-			is >> other.v[i];
-		}
-		return is;
-	}
+	return is;
+  }
 };
 
 template <class T>
-TVector<T>::TVector(int _n) : n(_n) {
-	if (n < 0)
-	{
-		MyException ex(1, "Некорректный размер вектора");
-		throw ex;
-	}
-	if (n == 0)
-		v = NULL;
-	else {
-		v = new T[n];
-		for (int i = 0; i < n; i++)
-			v[i] = 0;
-	}
-}
+TVector<T>::TVector(int _n) : n(_n) 
+{
+  if (n < 0)
+  {
+	MyException ex(1, "Некорректный размер вектора");
+	throw ex;
+  }
 
-template <class T>
-TVector<T>::TVector(const TVector &other) : n(other.n) {
+  if (n == 0)
+	v = NULL;
+  
+  else 
+  {
 	v = new T[n];
 	for (int i = 0; i < n; i++)
-		v[i] = other.v[i];
+	  v[i] = 0;
+  }
 }
 
 template <class T>
-T& TVector<T>::operator[](int i) {
-	if (i < 0 || i >= n)
-	{
-		MyException ex(4, "Элемента не существует");
-		throw ex;
-	}
-	return v[i];
+TVector<T>::TVector(const TVector &other) : n(other.n) 
+{
+  v = new T[n];
+  for (int i = 0; i < n; i++)
+	v[i] = other.v[i];
 }
 
 template <class T>
-TVector<T>& TVector<T>::operator=(const TVector &other) {
-	if (this == &other)
-		return *this;
-	if (n != other.n) {
-		delete[] v;
-		n = other.n;
-		v = new T[n];
-	}
-	for (int i = 0; i < n; i++)
-		v[i] = other.v[i];
+T& TVector<T>::operator[](int i) 
+{
+  if (i < 0 || i >= n)
+  {
+	MyException ex(4, "Элемента не существует");
+	throw ex;
+  }
+  return v[i];
+}
+
+template <class T>
+TVector<T>& TVector<T>::operator=(const TVector &other) 
+{
+  if (this == &other)
 	return *this;
+  
+  if (n != other.n)
+  {
+	delete[] v;
+    n = other.n;
+	v = new T[n];
+  }
+
+  for (int i = 0; i < n; i++)
+	v[i] = other.v[i];
+  
+  return *this;
 }
 
 template <class T>
 TVector<T> TVector<T>::operator+()const { return *this; }
 
 template <class T>
-TVector<T> TVector<T>::operator++() {
-	for (int i = 0; i < n; i++)
-		v[i]++;
-	return *this;
+TVector<T> TVector<T>::operator++() 
+{
+  for (int i = 0; i < n; i++)
+	v[i]++;
+
+  return *this;
 }
 
 template <class T>
-TVector<T> TVector<T>::operator++(int) {
-	TVector<T> temp(*this);
-	for (int i = 0; i < n; i++)
-		++v[i];
-	return temp;
+TVector<T> TVector<T>::operator++(int) 
+{
+  TVector<T> temp(*this);
+  for (int i = 0; i < n; i++)
+	++v[i];
+
+  return temp;
 }
 
 template <class T>
-TVector<T> TVector<T>::operator-()const {
-	TVector<T> temp(n);
-	for (int i = 0; i < n; i++)
-		temp.v[i] = -v[i];
-	return temp;
+TVector<T> TVector<T>::operator-()const 
+{
+  TVector<T> temp(n);
+  for (int i = 0; i < n; i++)
+	temp.v[i] = -v[i];
+
+  return temp;
 }
 
 template <class T>
-TVector<T> TVector<T>::operator--() {
-	for (int i = 0; i < n; i++)
-		v[i]--;
-	return *this;
+TVector<T> TVector<T>::operator--() 
+{
+  for (int i = 0; i < n; i++)
+	v[i]--;
+
+  return *this;
 }
 
 template <class T>
-TVector<T> TVector<T>::operator--(int) {
-	TVector<T> temp(*this);
-	for (int i = 0;i < n;i++)
-		--v[i];
-	return temp;
+TVector<T> TVector<T>::operator--(int) 
+{
+  TVector<T> temp(*this);
+  for (int i = 0;i < n;i++)
+	 --v[i];
+
+  return temp;
 }
 
 template <class T>
-TVector<T> TVector<T>::operator+(const TVector &other) {
-	if (n != other.n)
-	{
-		MyException ex(5, "Невозможно выполнить операцию для векторов разной размерности");
-		throw ex;
-	}
-	TVector<T> temp(n);
-	for (int i = 0; i < n; i++)
-		temp.v[i] = v[i] + other.v[i];
-	return temp;
+TVector<T> TVector<T>::operator+(const TVector &other) 
+{
+  if (n != other.n)
+  {
+	  MyException ex(5, "Невозможно выполнить операцию для векторов разной размерности");
+	  throw ex;
+  }
+
+  TVector<T> temp(n);
+  for (int i = 0; i < n; i++)
+	temp.v[i] = v[i] + other.v[i];
+
+  return temp;
 }
 
 template <class T>
-TVector<T> TVector<T>::operator-(const TVector &other) {
-	if (n != other.n)
-	{
-		MyException ex(5, "Невозможно выполнить операцию для векторов разной размерности");
-		throw ex;
-	}
-	TVector<T> temp(n);
-	for (int i = 0; i < n; i++)
-		temp.v[i] = v[i] - other.v[i];
-	return temp;
+TVector<T> TVector<T>::operator-(const TVector &other) 
+{
+  if (n != other.n)
+  {
+	  MyException ex(5, "Невозможно выполнить операцию для векторов разной размерности");
+	  throw ex;
+  }
+
+  TVector<T> temp(n);
+  for (int i = 0; i < n; i++)
+	temp.v[i] = v[i] - other.v[i];
+
+  return temp;
 }
 
 template <class T>
-TVector<T> TVector<T>::operator*(const TVector &other) {
-	if (n != other.n)
-	{
-		MyException ex(5, "Невозможно выполнить операцию для векторов разной размерности");
-		throw ex;
-	}
-	TVector<T> temp(n);
-	for (int i = 0; i < n; i++)
-		temp.v[i] = v[i] * other.v[i];
-	return temp;
+TVector<T> TVector<T>::operator*(const TVector &other) 
+{
+  if (n != other.n)
+  {
+	  MyException ex(5, "Невозможно выполнить операцию для векторов разной размерности");
+	  throw ex;
+  }
+
+  TVector<T> temp(n);
+  for (int i = 0; i < n; i++)
+	temp.v[i] = v[i] * other.v[i];
+
+  return temp;
 }
 
 template <class T>
-TVector<T> TVector<T>::operator*(T ch) {
-	TVector<T> temp(n);
-	for (int i = 0;i < n;i++)
-		temp.v[i] = ch * v[i];
-	return temp;
+TVector<T> TVector<T>::operator*(T ch) 
+{
+  TVector<T> temp(n);
+  for (int i = 0;i < n;i++)
+	temp.v[i] = ch * v[i];
+
+  return temp;
 }
 
 template <class T>
-bool TVector<T>::operator==(const TVector &other) {
-	if (n != other.n)
+bool TVector<T>::operator==(const TVector &other) 
+{
+  if (n != other.n)
+	  return 0;
+  
+  for (int i = 0; i < n;i++)
+	if (v[i] != other.v[i])
 		return 0;
-	for (int i = 0; i < n;i++)
-		if (v[i] != other.v[i])
-			return 0;
+
+  return 1;
+}
+
+template <class T>
+bool TVector<T>::operator!=(const TVector &other) 
+{
+  if (n != other.n)
 	return 1;
+
+  for (int i = 0;i < n;i++)
+	if (v[i] != other.v[i])
+	  return 1;
+
+  return 0;
 }
 
 template <class T>
-bool TVector<T>::operator!=(const TVector &other) {
-	if (n != other.n)
-		return 1;
-	for (int i = 0;i < n;i++)
-		if (v[i] != other.v[i])
-			return 1;
-	return 0;
-}
+TVector<T>::~TVector() 
+{
+  if (v != NULL) 
+  {
+	  delete[] v;
+	  v = NULL;
+  }
 
-template <class T>
-TVector<T>::~TVector() {
-	if (v != NULL) {
-		delete[] v;
-		v = NULL;
-	}
-	n = 0;
+  n = 0;
 }
