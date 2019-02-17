fb4860998ffdc2fcfe825f9d3b27746206b39f31
KornevNikita <copperskylark@gmail.com>
2019-02-17 17:54:58 +0300

    add stacklist

--- ReversePolishNotation/main.cpp
+++ ReversePolishNotation/main.cpp
@@ -3,10 +3,10 @@
 
 int main() 
 {
-  string str = "1+1+1+1+1+1";
+  string str = "8/4";
   TQueue<char> q1(str.size());
   q1 = ToRPN(str);
-  for (int i = 0; i < (str.size() - 3); i++)
-	cout << "i =" <<i<< " " << q1.Get()<< endl;
+  double a = Res(q1);
+  cout << a;
   return 0;
 }
