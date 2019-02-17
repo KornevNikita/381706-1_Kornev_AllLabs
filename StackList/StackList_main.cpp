f9e6e10e38708d7c069908b564eae5558da3d9d9
KornevNikita <copperskylark@gmail.com>
2019-02-17 20:45:43 +0300

    edits

--- StackList/StackList_main.cpp
+++ StackList/StackList_main.cpp
@@ -5,13 +5,11 @@ int main()
   TStackList<double> li(10);
   li.Put(1);
   li.Put(2);
-  li.PutBegin(3);
-  li.PutEnd(4);
+  li.Put(3);
+  li.Put(4);
 
   double a, b, c, d, e, f;
   a = li.Get();
-  b = li.GetBegin();
-  c = li.GetEnd();
   d = li.GetSize();
   e = li.IsEmpty();
   f = li.IsFull();
