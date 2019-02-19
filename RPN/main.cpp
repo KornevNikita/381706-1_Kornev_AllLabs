8e55a888378d22830cfde258bbf97159fe81238c
KornevNikita <copperskylark@gmail.com>
2019-02-19 10:40:21 +0300

    edits

--- RPN/main.cpp
+++ RPN/main.cpp
@@ -4,7 +4,7 @@ int  main()
 {
   try
   {
-    TString s(1+2+3+4+5+6+7+8+9+10);
+    TString s("1+2+3+4+5+6+7+8+9+10");
     TQueue<char> B;
     B = ToRPN(s);
     cout << " = " << Result(B) << endl;
